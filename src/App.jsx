f9688bc21f87af2b9a38b9d4d449c75dacdebeed
Tom-Roche-Oclock <github-oclock@kinoah.com>
2023-07-26 08:49:19 +0200

    refactor: clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Button, Card } from '@nextui-org/react';
+import { Button, Card } from '@nextui-org/react'
 import './App.css'
 
 // data 
@@ -9,12 +9,12 @@ const words = [
   'blaireau',
   'biche',
   'ours',
-];
+]
 
 // mélanger un tableau (simpliste)
 function shuffle(array) {
   const shuffledArray = [...array]
-  return shuffledArray.sort(() => Math.random() - 0.5);
+  return shuffledArray.sort(() => Math.random() - 0.5)
 }
 
 function App() {
@@ -22,17 +22,18 @@ function App() {
   const [guessedWord, setGuessedWord] = useState([])
   const [keyboard, setKeyboard] = useState([])
 
+  // au chargement
   useEffect(() => {
-    // random word depuis la data
+    // random word depuis la data, passé en capitale
     const randomWord = words[Math.floor(Math.random() * words.length)].toUpperCase()
 
     // définir les states
-    setWord(randomWord.split(''))
-    setGuessedWord([...randomWord].fill('_', 0))
+    setWord([...randomWord])
+    setGuessedWord([...randomWord].fill('_'))
 
     // Chaque key du keyboard est un objet avec 2 propriétés
     // Key { used: false, letter: 'A' }
-    setKeyboard(shuffle(randomWord.split('')).map((letter) => {
+    setKeyboard(shuffle([...randomWord]).map((letter) => {
       return {
         used: false,
         letter: letter
@@ -69,7 +70,11 @@ function App() {
     <div className="App">
       <div className="guessed-word">
         { guessedWord.map((letter, index) => (
-          <Card key={index} className='letter'>{letter}</Card>
+          <Card
+          key={index}
+          className='letter'>
+            {letter}
+          </Card>
         )) }
       </div>
       <div className='keyboard'>
